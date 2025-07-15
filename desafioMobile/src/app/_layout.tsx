c229536b7735d3d911d985a85e683f071def50b6
gustavoaesteves <gustavoalves19992@gmail.com>
2025-07-15 02:58:15 -0300

    feat: adiçao do Provider context e controle de rotas

--- desafioMobile/src/app/_layout.tsx
+++ desafioMobile/src/app/_layout.tsx
@@ -1,11 +1,17 @@
 ﻿import { Stack } from 'expo-router'
 import React from 'react'
+
 import '../../global.css'
+import { QuizProvider } from '../../contexts/QuizContext'
 
 export default function Layout(): React.JSX.Element {
 	return (
-		<Stack screenOptions={{ headerShown: false }}>
-			<Stack.Screen name="sign-in" />
-        </Stack>
+		<QuizProvider>
+			<Stack screenOptions={{ headerShown: false }}>
+				<Stack.Screen name="sign-in" />
+				<Stack.Screen name="(quiz)" />
+				<Stack.Screen name="results" />
+			</Stack>
+		</QuizProvider>
 	)
 }
