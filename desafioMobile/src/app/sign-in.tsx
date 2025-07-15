f6f152129d044f1e2b7c9660f025fb53a17ff299
gustavoaesteves <gustavoalves19992@gmail.com>
2025-07-15 02:58:52 -0300

    feat: tela base de login

--- desafioMobile/src/app/sign-in.tsx
+++ desafioMobile/src/app/sign-in.tsx
@@ -1,28 +1,40 @@
-﻿import { router } from 'expo-router'
-import React from 'react'
-import { LinearGradient } from 'expo-linear-gradient'
-import { AppColors } from '../../constants/Colors'
+﻿import { LinearGradient } from 'expo-linear-gradient'
+import { router } from 'expo-router'
+import React, { useEffect } from 'react'
+import { SafeAreaView } from 'react-native'
+
 import { Button } from '../../components/Button'
+import { AppColors } from '../../constants/Colors'
+import { useQuiz } from '../../contexts/QuizContext'
 
 export default function SignIn(): React.JSX.Element {
+	const { resetQuiz } = useQuiz()
+
+	useEffect(() => {
+		// Reset do quiz quando entrar na tela de sign-in
+		resetQuiz()
+	}, [resetQuiz])
+
 	function signIn(): void {
 		router.navigate('/(quiz)/quiz')
 	}
 
 	return (
-		<LinearGradient
-			colors={[AppColors.primary, AppColors.secondary]}
-			start={{ x: 0, y: 0.7 }}
-			end={{ x: 1, y: 0.3 }}
-			className="flex-1 justify-center items-center p-4"
-		>
-			<Button
-				title="Acessar Quiz"
-				onPress={signIn}
-                variant="primary"
-				size="xl"
-				customClasses="rounded-xl"
-			/>
-		</LinearGradient>
+		<SafeAreaView className="flex-1">
+			<LinearGradient
+				colors={[AppColors.primary, AppColors.secondary]}
+				start={{ x: 0, y: 0.7 }}
+				end={{ x: 1, y: 0.3 }}
+				className="flex-1 justify-center items-center p-4"
+			>
+				<Button
+					title="Acessar Quiz"
+					onPress={signIn}
+					variant="primary"
+					size="xl"
+					customClasses="rounded-xl"
+				/>
+			</LinearGradient>
+		</SafeAreaView>
 	)
 }
